a4dd13511d2e74b90aa89b839065aadaa72c8e21
TheDreadedAndy <andyespaulding@gmail.com>
2018-04-29 20:06:40 -0400

    first commit

---
+++ LovelyUploadBot.py
@@ -0,0 +1,177 @@
+import praw # Reddit API
+import time # So I don't spam google's servers and kill my data
+import os
+from googleapiclient.discovery import build # Youtube API
+from googleapiclient.errors import HttpError # Youtube Errors
+
+VERBOSE = True # Setting to false suppresses mundane information and most warnings.
+GRUMPS_NAME = 'Game Grumps'
+GRUMPS_PL = 'UU9CuvdOVfMPvKCiwdGKL3cQ' # Uploads playlist of Game Grumps YouTube channel.
+GRUMPS_SUBREDDIT = 'gamegrumps' # Subreddit new videos will be submitted to.
+GRUMPOUT_NAME = 'Grump Out'
+GRUMPOUT_PL = 'UUAQ0o3l-H3y_n56C3yJ9EHA' # Uploads playlist for Grump Out.
+VID_TITLE = 0
+VID_URL = 1
+
+def main():
+    verbose('Initializing...', override=True)
+    youtube, reddit = initAPIs()
+    if youtube == None and reddit == None: return
+    gameGrumps = Youtuber()
+    grumpOut = Youtuber()
+    initYoutuber(youtube, reddit, gameGrumps, GRUMPS_NAME, GRUMPS_PL, GRUMPS_SUBREDDIT)
+    initYoutuber(youtube, reddit, grumpOut, GRUMPOUT_NAME, GRUMPOUT_PL, GRUMPS_SUBREDDIT)
+    verbose('Done!', override=True)
+    # Checks for new videos and uploads them when they appear.
+    while True:
+        try:
+            verbose("Checking for new uploads...")
+            updateYoutuber(youtube, gameGrumps)
+            updateYoutuber(youtube, grumpOut)
+            time.sleep(abs(60 - time.localtime(time.time()).tm_sec))
+        except KeyboardInterrupt:
+            verbose("Keyboard interrupt received, ending violently.", override=True)
+            return
+        except Exception as e:
+            verbose(e, override=True)
+            return
+
+def verbose(s, override=False):
+    timeStamp = "[%s]:" % (time.strftime("%I:%M:%S %p",time.localtime(time.time())))
+    if VERBOSE or override: print(timeStamp, s)
+
+def initAPIs():
+    try:
+        # See README for more information.
+        path = os.path.dirname(os.path.abspath(__file__)) + '/keys.txt'
+        with open(path, 'rt') as file:
+            keys = file.read().splitlines()
+            YT_API_KEY = keys[0]
+            REDDIT_ID = keys[1]
+            REDDIT_SECRET = keys[2]
+            REDDIT_AGENT = keys[3]
+            REDDIT_USER = keys[4]
+            REDDIT_PASS = keys[5]
+    except Exception as e:
+        verbose(e, override=True)
+        verbose("FATAL: Problem opening keys.txt! Please view the ReadMe for help.", override=True)
+        return None, None
+    # This gives me an object that lets me deal with youtube.
+    youtube = build("youtube", "v3", developerKey=YT_API_KEY)
+    # This object lets me interact with reddit.
+    reddit = praw.Reddit(client_id=REDDIT_ID, client_secret=REDDIT_SECRET,\
+                        user_agent=REDDIT_AGENT,\
+                        username=REDDIT_USER, password=REDDIT_PASS)
+    return youtube, reddit
+
+def initYoutuber(youtube, reddit, youtuber, name, uploadPl, sub):
+    youtuber.subreddit = reddit.subreddit(sub)
+    youtuber.name = name
+    # I use this number to track when a video is added.
+    youtuber.numVids = youtube.playlistItems().list(part="snippet, ContentDetails", maxResults=3,\
+                                playlistId=uploadPl).execute()['pageInfo']['totalResults']
+    youtuber.pl = uploadPl
+    # The video list is used later to track when a new video is uploaded.
+    youtuber.vidList = initVidList(youtube, youtuber)
+    verbose("Initialized channel %s." % (youtuber.name), override=True)
+
+def initVidList(youtube, youtuber):
+    # Generates a list of every upload from the given channel.
+    https, pl = getPlaylistItems(youtube, youtuber)
+    moreVids = True
+    vidList = set()
+    while moreVids:
+        # Adds the video id of each video in the page to the set.
+        for item in pl['items']:
+            id = item['contentDetails']['videoId']
+            vidList.add(id)
+        # Prevents the loop from ending until we know the ID of every video.
+        if len(vidList) >= youtuber.numVids: moreVids = False
+        # Functionally, this is an if statement that checks if there is another page.
+        # Conceptually, this is terrible code.
+        try:
+            pl['nextPageToken']
+            https, pl = getPlaylistItems(youtube, youtuber, https=https, pl=pl)
+        except:
+            if moreVids:
+                verbose('Warning: Loop reached while generating video list!')
+                https, pl = getPlaylistItems(youtube, youtuber)
+    return vidList
+
+# A decorator that ensures getPlaylistItems() will execute until it gets a response.
+def ensureConnection(f):
+    def g(*args, **kwargs):
+        sleep = 1
+        while True:
+            try: return f(*args, **kwargs)
+            except improperUsageError as e: raise e
+            except:
+                verbose("Warning! Connection error while getting playlist items!",\
+                                                                    override=True)
+                verbose("Retrying in %d seconds..." % (sleep), override=True)
+                time.sleep(sleep)
+                sleep *= 2
+    return g
+
+@ensureConnection
+def getPlaylistItems(youtube, youtuber, https=None, pl=None):
+    if https == None and pl == None:
+        https = youtube.playlistItems().list(part="snippet, ContentDetails",\
+                                        maxResults=50, playlistId=youtuber.pl)
+    elif https != None and pl != None:
+        https = youtube.playlistItems().list_next(https, pl)
+    else:
+        raise improperUsageError("getPlaylistItems() requires that either both"\
+                                 + " or neither https/pl be set!")
+    return https, https.execute()
+
+def updateYoutuber(youtube, youtuber):
+    newUploads = getLatestVideo(youtube, youtuber)
+    if newUploads:
+        verbose("Found new content uploaded by %s!" % (youtuber.name), override=True)
+        for vid in newUploads:
+            try:
+                youtuber.subreddit.submit(title=vid[VID_TITLE], url=vid[VID_URL],\
+                                          resubmit=True, send_replies=False)
+                verbose('Submitted video \"%s\"' % (vid[VID_TITLE]), override=True)
+            except Exception as e:
+                verbose(e, override=True)
+    else:
+        verbose("No new uploads found for channel %s." % (youtuber.name))
+
+def getLatestVideo(youtube, youtuber):
+    # This function checks if the total number of videos in the upload playlist
+    # has increased, and returns the new videos if so.
+    https, pl = getPlaylistItems(youtube, youtuber)
+    newNumVids = pl['pageInfo']['totalResults']
+    if newNumVids <= youtuber.numVids: return False
+    dVids = newNumVids - youtuber.numVids
+    youtuber.numVids = newNumVids
+    i = 0
+    newUploads = []
+    while i < dVids:
+        # Searches each page of the playlist for new videos until it has found them all.
+        for item in pl['items']:
+            id = item['contentDetails']['videoId']
+            if id not in youtuber.vidList:
+                title = item['snippet']['title']
+                url = "https://www.youtube.com/watch?v=%s" % (id)
+                youtuber.vidList.add(id)
+                newUploads.append((title, url))
+                i += 1
+        # Functionally, this is an if statement that checks if there is another page.
+        # Conceptually, this is terrible code.
+        try:
+            pl['nextPageToken']
+            https, pl = getPlaylistItems(youtube, youtuber, https=https, pl=pl)
+        except:
+            if i < dVids:
+                verbose('Warning: Loop reached while searching for latest video!')
+                https, pl = getPlaylistItems(youtube, youtuber)
+    return newUploads
+
+class Youtuber(object): pass
+
+class improperUsageError(Exception): pass
+
+if __name__ == "__main__": main()
---
+++ README.md
@@ -0,0 +1,20 @@
+#The Rumor Come Out: Does /u/General_Scales is Bot?
+
+LovelyUploadBot is a python script that pulls videos from a playlist and uploads them to Reddit, whenever new videos are added. The obvious application for this bot is it's usage on a channels upload playlist, which is why it was created.
+
+As is, the code is written to be run on the Game Grumps subreddit with the Game Grumps channel, however this should be easy to change. Should you wish to run this bot, you will need Python 3, PRAW, and the Google Python API.
+
+Install Python 3, then run:
+sudo pip install praw
+sudo pip install --upgrade google-api-python-client
+
+Additionally, you will need a Youtube API key from developers.google.com. Next, you will need to create a Reddit project for your bot, and collect its ID and Secret. More information can be found in the documentation for PRAW. This information will need to be compiled into a text file of the following format, named "keys.txt":
+
+<Youtube API Key>
+<Reddit ID>
+<Reddit Secret>
+<Reddit Agent>
+<Reddit Username>
+<Reddit Password>
+
+Finally, you will need to edit the main function of the bot. Simply remove the lines declaring, initializing, and updating the Game Grumps/GrumpOut channel in the main function and replace them with whichever channels you wish to use.
